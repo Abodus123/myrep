28067d76bcb4e0ec7218b976fb7a9021ab8368f6
sanyagey <mrmishamorozov@gmail.com>
2026-01-17 13:29:53 +0300

    pynkt2

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print('Приветствую всех')
 tasks = []
+def see_tasks():
+    if not tasks:
+        print('Задач нету')
+    else:
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
