afe2cf185ccf9dde0aa5368c06ab601df17db7a4
sanyagey <mrmishamorozov@gmail.com>
2026-01-17 13:54:56 +0300

    pynkt5

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ def see_tasks():
         for i, task in enumerate(tasks, 1):
             print(f"{i}. {task}")
 
+def del_task(number_task):
+    if 0 < number_task < len(tasks):
+        tasks.pop(number_task - 1)
+        print('задача удалена')
+    else:
+        print('Такой задачи не сущетствует')
+
+
+
 def task_add(new_task):
     tasks.append(new_task)
     print('Задача добавленна')
 
 while True:
-    answer = int(input('Выберите действие. 1- показать, 2 -добавить, 0 - выйти'))
+    answer = int(input('Выберите действие. 1- показать, 2 -добавить, 3 - выйти, 4 - удалить'))
     if answer == 1:
         see_tasks()
     elif answer == 2:
@@ -20,5 +29,8 @@ while True:
         task_add(new_task)
     elif answer == 3:
         break
+    elif answer == 4:
+        number_task = int(input('Введите номер задачи'))
+        del_task(number_task)
     else:
         print('Вы ввели не правильную цифру')
